c02936f3d23d7d11465a42562da167d9cfd2f5a1
Ma-hawaj <ma.hawaj@gmail.com>
2024-09-10 19:17:02 +0300

    organizing nvim config

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,12 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
